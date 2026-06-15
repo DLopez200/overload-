5f12c1969f75c7d74d468a3b846a9628484ce7a7
DLopez200 <ld30930@mail.harpercollege.edu>
2026-06-14 21:40:07 -0500

    review
    using the addition class

--- overload++.cpp
+++ overload++.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
 using namespace std;
 
+class number {
+    private:
+    int num;
 
+    public:
+    
+
+    number (int x){
+        num = x;
+    }
+    number operator+(const number& b){
+        return number(num + b.num);
+
+    }
+
+    void print(){
+        cout << num << endl;
+    }
+
+
+};
 
 int main(){
+
+    /*a.f(b);the same
+        a+b*/
+
+        number a = (2);
+        number b = (4);
+
+        (a + b).print();
 
     return 0;
 }
