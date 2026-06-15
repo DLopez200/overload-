e36953be4497229db453568edc897515673028fa
DLopez200 <ld30930@mail.harpercollege.edu>
2026-06-14 21:57:44 -0500

    a++ and ++a increment (end)
    crated function to do a++ increments

--- overload++.cpp
+++ overload++.cpp
@@ -20,7 +20,15 @@ class number {
         cout << num << endl;
     }
 
-    number& operator++(){
+    number operator++(){
+        num++;
+        return number(num);//for ++a
+    }
+
+    number operator++(int){ //used for a++
+        number old = number(num);
+        num++;
+        return old;
 
     }
 
@@ -39,12 +47,22 @@ int main(){
         (a + b).print();
 
         //a++; // a--
-        //++a; // --a
+        ++a; // --a
 
+        a.print();
+
+        a++;
+        a.print();
+    /* DONT DO THIS
+    ANY ASSIGNENT SHOULD BE ITS OWN LINE OF CODE
         int x = 4;
-
         cout << x++ << endl;
         cout << ++x << endl;
+if you wanted to incrament it do this:
+        x++
+        cout << x<<endl;
+    */
+
 
     return 0;
 }
